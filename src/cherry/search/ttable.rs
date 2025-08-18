3208af4ffde7b16e163c8f24ed89442d0401273c
Tecci <tecci.fi@gmail.com>
2025-08-18 20:12:21 +0300

    replacement stuff

--- src/cherry/search/ttable.rs
+++ src/cherry/search/ttable.rs
@@ -202,8 +202,10 @@ impl TTable {
         let hash = board.hash();
         let index = self.index(hash);
 
+        let old_hash = self.entries[index].key.load(Ordering::Relaxed);
         let old_data = self.entries[index].data();
-        if old_data.bound == TTBound::None || new_data.age != old_data.age {
+
+        if Self::replace(&old_data, &new_data, old_hash, hash) {
             self.entries[index].set(hash, new_data);
         }
     }
@@ -232,6 +234,11 @@ impl TTable {
     }
 
     /*----------------------------------------------------------------*/
+
+    #[inline]
+    fn replace(old_data: &TTData, new_data: &TTData, old_hash: u64, new_hash: u64) -> bool {
+        old_data.bound == TTBound::None || old_hash != new_hash || old_data.age != new_data.age
+    }
 
     #[inline]
     fn index(&self, hash: u64) -> usize {
