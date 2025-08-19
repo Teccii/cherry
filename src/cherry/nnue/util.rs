f870ff12550e747f384e927c7012da04d3bb64bd
Tecci <tecci.fi@gmail.com>
2025-08-19 18:16:13 +0300

    revert chunk size thing

--- src/cherry/nnue/util.rs
+++ src/cherry/nnue/util.rs
@@ -23,18 +23,15 @@ impl<T> std::ops::DerefMut for Align64<T> {
 
 /*----------------------------------------------------------------*/
 
-#[cfg(target_feature = "avx512f")] pub const REGISTER_SIZE: usize = 64;
+#[cfg(target_feature = "avx512f")] pub const CHUNK_SIZE: usize = 64;
 #[cfg(all(
     target_feature = "avx2",
     not(target_feature = "avx512f"))
-)] pub const REGISTER_SIZE: usize = 32;
+)] pub const CHUNK_SIZE: usize = 32;
 #[cfg(all(
     not(target_feature = "avx2"),
     not(target_feature = "avx512f"))
-)] pub const REGISTER_SIZE: usize = 16;
-
-pub const CHUNK_SIZE: usize = REGISTER_SIZE / size_of::<i16>();
-
+)] pub const CHUNK_SIZE: usize = 16;
 pub type I16Reg = Simd<i16, CHUNK_SIZE>;
 pub type I32Reg = Simd<i32, CHUNK_SIZE>;
 
