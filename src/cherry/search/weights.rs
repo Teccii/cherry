bbf51e1b3abd3a8897e503e82c580e0296640544
Tecci <tecci.fi@gmail.com>
2026-04-19 03:49:47 +0300

    Hindsight Extension
    Bench: 4696544
    
    ```
    Elo   | 2.25 +- 1.75 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | 2.90 (-2.25, 2.89) [0.00, 3.00]
    Games | N: 41822 W: 9963 L: 9692 D: 22167
    Penta | [167, 4936, 10452, 5171, 185]
    ```
    https://openbench.nocturn9x.space/test/6733/
    
    very trustable test waow

--- src/cherry/search/weights.rs
+++ src/cherry/search/weights.rs
@@ -189,6 +189,10 @@ weights! {
     mat_scale_rook   | MAT_SCALE_ROOK:   i32 => 565   | 400..=700;
     mat_scale_queen  | MAT_SCALE_QUEEN:  i32 => 981   | 700..=1100;
     mat_scale_base   | MAT_SCALE_BASE:   i32 => 25242 | 24000..=26000;
+    
+    hindsight_ext_eval | HINDSIGHT_EXT_EVAL: i32 => 0    | -100..=0;
+    hindsight_ext_red  | HINDSIGHT_EXT_RED:  i32 => 3072 | 2048..=4096;
+    hindsight_ext      | HINDSIGHT_EXT:      i32 => 1024 | 512..=1536;
 
     rfp_depth      | RFP_DEPTH:      i32 => 6550 | 4096..=8192;
     rfp_base       | RFP_BASE:       i64 => 5    | -100..=100;
